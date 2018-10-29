c487957cd6f1d67301f0b81dc2d1c2ce9811e1d0
huskyhoochu <dfg1499@gmail.com>
2018-10-29 16:43:09 +0900

    [#1] infix, prefix, postfix 구분하는 함수 설계
    - 식의 모든 요소를 쪼개 deque에 저장
    - deque에서 요소를 하나씩 꺼내보면서 연산자와 숫자 순서에 따라 표기법 판단

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+from collections import deque
+from itertools import chain
 
 
 def validate(equation, x_value):
@@ -19,7 +21,41 @@ def validate(equation, x_value):
     return True
 
 
-def evaluate_normal(equation, x_value):
+def make_deque(equation):
+    _, expression = equation.split('=')
+    strip_ex = expression.replace(' ', '')
+    return deque(chain(strip_ex))
+
+
+def select_notations(collection):
+    brackets = ('(', ')')
+    operators = ('+', '-', '*', '/')
+    operator_count = 0
+    num_count = 0
+
+    while True:
+        token = collection.popleft()
+        if token in brackets:
+            continue
+
+        if token in operators:
+            if num_count == 0:
+                operator_count += 1
+                continue
+            elif num_count == 1:
+                return 'infix'
+            elif num_count >= 2:
+                return 'postfix'
+
+        else:
+            if operator_count == 0:
+                num_count += 1
+                continue
+            elif operator_count >= 1:
+                return 'prefix'
+
+
+def evaluate_infix(equation, x_value):
     _, expression = equation.split('=')
     replaced_ex = expression.replace('x', x_value)
     return eval(replaced_ex)
@@ -38,7 +74,18 @@ def main():
         if is_validated is False:
             continue
 
-        answer = evaluate_normal(equation, x_value)
+        deque_eq = make_deque(equation)
+
+        notation = select_notations(deque_eq)
+
+        if notation is 'prefix':
+            pass
+        elif notation is 'infix':
+            pass
+        elif notation is 'postfix':
+            pass
+
+        answer = evaluate_infix(equation, x_value)
         print(f'y: {answer}')
         time.sleep(1)
 
