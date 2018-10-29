69bbae56e60933f4b0f12d08f6c627feca050eb2
huskyhoochu <dfg1499@gmail.com>
2018-10-29 15:33:57 +0900

    [#1] evaluate, validate 메서드 분리
    - 식이 잘못 입력된 경우, x가 숫자가 아닌 경우 에러 처리
    - 등식인지, 표현식인지 판단하는 조건문 추가

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 import time
 
 
+def validate(equation, x_value):
+    if 'x' not in equation:
+        print('Please type "x" in equation')
+        return False
+
+    try:
+        int(x_value)
+    except ValueError:
+        print('x is not a number')
+        return False
+
+    return True
+
+
 def evaluate(equation, x_value):
-    y, expression = equation.split('=')
-    replaced_eq = expression.replace('x', x_value)
-    return eval(replaced_eq)
+    if '=' not in equation:
+        replaced_eq = equation.replace('x', x_value)
+        return eval(replaced_eq)
+
+    _, expression = equation.split('=')
+    replaced_ex = expression.replace('x', x_value)
+    return eval(replaced_ex)
 
 
 def main():
     while True:
-        equation = input('please input equation(quit: q): ')
-
+        equation = input('Please input equation(quit: q): ')
         if equation == 'q':
-            print('bye')
+            print('Bye')
             break
 
-        x_value = input('please input X: ')
+        x_value = input('Please input x: ')
+
+        is_validated = validate(equation, x_value)
+        if is_validated is False:
+            continue
+
         answer = evaluate(equation, x_value)
-        print(f'Y: {answer}')
+        print(f'y: {answer}')
         time.sleep(1)
 
 
