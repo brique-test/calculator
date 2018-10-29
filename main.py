9ac129580dc0e6839419d5fe721423c3e787ae74
huskyhoochu <dfg1499@gmail.com>
2018-10-29 15:02:03 +0900

    [#1] 실행 메서드 /  계산 메서드 분리

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-if __name__ == '__main__':
-    equation = input('please input equation: ')
-    x = input('please input X: ')
+import time
+
+
+def evaluate(equation, x_value):
     y, expression = equation.split('=')
-    replaced_eq = expression.replace('x', x)
-    answer = eval(replaced_eq)
-    print(f'Y: {answer}')
+    replaced_eq = expression.replace('x', x_value)
+    return eval(replaced_eq)
+
+
+def main():
+    while True:
+        equation = input('please input equation(quit: q): ')
+
+        if equation == 'q':
+            print('bye')
+            break
+
+        x_value = input('please input X: ')
+        answer = evaluate(equation, x_value)
+        print(f'Y: {answer}')
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
